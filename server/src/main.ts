fe3aba25f578bd57ecd8da8ca2d8c1832eab686d
risv1 <rishi.aangarai@gmail.com>
2024-06-28 08:13:45 +0530

    refactor: server dir

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,10 +3,11 @@ import bodyParser from "body-parser";
 import routes from "./routes/routes";
 
 const app = express();
+const port = process.env.SERVER_PORT || 8000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(routes);
 
-app.listen(8000, () => {
+app.listen(port, () => {
   console.log("Server is running on port 8000");
 });
