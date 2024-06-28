c9e986d3f12b888ee2bc1b2fb0d6a25edf91cf37
risv1 <rishi.aangarai@gmail.com>
2024-06-28 08:09:58 +0530

    feat: add client

---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            <h1 className="font-bold text-xl text-black">Hello World</h1>
+        </div>
+    )
+}
